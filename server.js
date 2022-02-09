963dcf6078488d789b4d5a32685ba0056fafa7cd
Ahmad-Mukhtar <66535485+Ahmad-Mukhtar@users.noreply.github.com>
2022-02-09 15:21:39 +0500

    Starteing Out

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter=require('./routes/index')
+
+const app = express();
+
+
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use('/',indexRouter)
+
+
+
+
+app.listen(process.env.PORT || 3000)
